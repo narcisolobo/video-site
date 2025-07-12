7b44b10a4c59e2e5c2a28c827dc0794178aa4137
narcisolobo <narcisolobo@gmail.com>
2025-07-12 14:17:03 -0700

    typed an any

--- next-app/src/lib/sanity.ts
+++ next-app/src/lib/sanity.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const client = createClient({
   projectId: "5oob6xe3",
@@ -9,7 +10,7 @@ const client = createClient({
 });
 
 const builder = imageUrlBuilder(client);
-function urlFor(source: any) {
+function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
 
